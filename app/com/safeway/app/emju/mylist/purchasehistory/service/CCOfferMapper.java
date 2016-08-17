5fac591b0386ce099ff5049414e6e4c3daea9d0f
edura22 <edura22@VAN1560089L.safeway01.ad.safeway.com>
2016-08-17 12:00:58 -0700

    #7296 Defect fix

--- app/com/safeway/app/emju/mylist/purchasehistory/service/CCOfferMapper.java
+++ app/com/safeway/app/emju/mylist/purchasehistory/service/CCOfferMapper.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 import com.safeway.app.emju.allocation.cliptracking.model.OfferClipStatus;
 import com.safeway.app.emju.allocation.helper.OfferConstants.ClipStatus;
-import com.safeway.app.emju.allocation.helper.OfferConstants.OfferStatus;
 import com.safeway.app.emju.allocation.helper.OfferConstants.PurchaseIndicator;
 import com.safeway.app.emju.cache.entity.OfferDetail;
 import com.safeway.app.emju.helper.DataHelper;
@@ -26,7 +25,7 @@ import com.safeway.app.emju.mylist.model.AllocatedOffer;
 public class CCOfferMapper {
 
 	private static final List<String> INVALID_STATUS_TYPES = Arrays.asList(new String[] { "L", "l", "O", "o", "I", "i",
-	        "D", "d", "AD", "ad" });
+	        "D", "d", "AD", "ad", "E", "e" });
 	
     private static final String HOUSEHOLD_TARGETED_OFFERS = "08";
 	private final static Logger LOGGER = LoggerFactory.getLogger(CCOfferMapper.class);
@@ -68,84 +67,22 @@ public class CCOfferMapper {
 
         String offerSubPgmTypeCd = offerDetail.getOfferSubProgram() == null ? "" : offerDetail.getOfferSubProgram();
         
-        if (offerInvalid && ClipStatus.UNCLIPPED.equalsIgnoreCase(clipStatus)) {
-            isAcceptableOffer = false;
+        if(clientDBCurrDt.before(displayStartDt) || clientDBCurrDt.after(displayEndDt)){
+        	
+        	isAcceptableOffer = false;
+        	
+        } else if(ClipStatus.UNCLIPPED.equalsIgnoreCase(clipStatus)) {
+        	
+        	if(offerInvalid) {
+        		
+        		isAcceptableOffer = false;
+        		
+        	} else if(offerSubPgmTypeCd.equalsIgnoreCase(HOUSEHOLD_TARGETED_OFFERS) && (!catalinaOffersAvailable || !catalinaAllocationOffers
+                    .contains(currentOfferId))) {
+        		
+        		isAcceptableOffer = false;
+        	}
         }
-        else if (!offerInvalid && ClipStatus.UNCLIPPED.equalsIgnoreCase(clipStatus)) {
-            if (clientDBCurrDt.before(displayStartDt)
-                || clientDBCurrDt.after(displayEndDt)
-                || (offerSubPgmTypeCd.equalsIgnoreCase(HOUSEHOLD_TARGETED_OFFERS) && (!catalinaOffersAvailable || !catalinaAllocationOffers
-                    .contains(currentOfferId)))) {
-                isAcceptableOffer = false;
-            }
-        }
-/*        
-		boolean offerValid = OfferStatus.ACTIVE.equals(offerStatus);
-		
-		boolean isNotOfferDateValid = clientDBCurrDt.before(displayStartDt) || clientDBCurrDt.after(displayEndDt);
-		
-
-        
-    	LOGGER.debug(">>>> offerStatus = " + offerStatus);
-		LOGGER.debug(">>>> offerInvalid = " + offerInvalid);
-		LOGGER.debug(">>>> offerValid = " + offerValid);
-
-		LOGGER.debug(">>>> isNotOfferDateValid = " + isNotOfferDateValid);
-		LOGGER.debug(">>>> clientDBCurrDt = " + clientDBCurrDt);
-		LOGGER.debug(">>>> offer displayStartDt = " + displayStartDt);
-		LOGGER.debug(">>>> offer displayEndDt = " + displayEndDt);
-		
-        
-        if (null == offerSubPgmTypeCd) {
-        	offerSubPgmTypeCd ="";
-        }
-        
-        
-        if (isNotOfferDateValid) {
-			isAcceptableOffer = false;
-		} else {
-		
-				//Clipped offer
-				if(ClipStatus.ADDED_TO_CARD.equalsIgnoreCase(clipStatus)) {
-					if(offerInvalid && isNotOfferDateValid) {//offer is clipped: Offer Status in  “L,D,E etc”:  end date of offer passed : not show Offer
-						isAcceptableOffer = false;
-					} 
-					
-					if (offerValid && isNotOfferDateValid) { //offer is clipped: Offer Status is A:  end date of offer passed 
-						isAcceptableOffer = false;
-					}
-					
-				}
-				
-				//Unclipped offer
-				if(ClipStatus.UNCLIPPED.equalsIgnoreCase(clipStatus)) {
-					
-					if(offerInvalid && !isNotOfferDateValid) {//offer is Unclipped: Offer Status in “L,D,E etc”: end date of offer not passed : don’t show Offer
-						isAcceptableOffer = false;
-					} 
-					
-					if (offerValid && isNotOfferDateValid) { //offer is Unclipped: Offer Status is A:  end date of offer passed : don’t show Offer 
-							isAcceptableOffer = false;
-					}
-				}
-		}
-		
-        LOGGER.debug(">>>> isAcceptableOffer = " + isAcceptableOffer);
-		
-		boolean isNotPartnerOfferValid = offerSubPgmTypeCd.equalsIgnoreCase(HOUSEHOLD_TARGETED_OFFERS) && (!catalinaOffersAvailable || !catalinaAllocationOffers
-                .contains(currentOfferId));
-		LOGGER.debug(">>>> isNotPartnerOfferValid = " + isNotPartnerOfferValid);
-		
-		if (isNotPartnerOfferValid) {
-			isAcceptableOffer = false;
-		}
-		
-		LOGGER.debug(">>>> isAcceptableOffer after = " + isAcceptableOffer);
-        
-        if(!isAcceptableOffer) {
-        	return null;
-        }
-*/
         
         if (isAcceptableOffer) {
 	        offer = new AllocatedOffer();
--- app/com/safeway/app/emju/mylist/purchasehistory/service/PDOfferMapper.java
+++ app/com/safeway/app/emju/mylist/purchasehistory/service/PDOfferMapper.java
@@ -35,7 +35,7 @@ public class PDOfferMapper {
 	
 
 	private static final List<String> INVALID_STATUS_TYPES = Arrays
-			.asList(new String[] { "L", "l", "O", "o", "I", "i", "D", "d", "AD", "ad" });
+			.asList(new String[] { "L", "l", "O", "o", "I", "i", "D", "d", "AD", "ad", "E", "e" });
 
 	/**
 	 * 
@@ -50,6 +50,7 @@ public class PDOfferMapper {
 		
 		LOGGER.debug(">>>> PDOfferMapper >>>>> " );
 
+		AllocatedOffer offer = null;
 		// get current DB current Date
 		Long clientDBCurrDtInMS = DataHelper.getCurrTsInTzAsDBTzMs(timeZone);
 	    Date clientDBCurrDt = new Date(clientDBCurrDtInMS);
@@ -75,110 +76,39 @@ public class PDOfferMapper {
 		boolean offerInvalid =  INVALID_STATUS_TYPES.contains(offerStatus);
 		boolean offerValid = OfferStatus.ACTIVE.equals(offerStatus);
 		
-		boolean isNotOfferDateValid = clientDBCurrDt.before(displayStartDt) || clientDBCurrDt.after(displayEndDt);
 		
-		LOGGER.debug(">>>> offerStatus = " + offerStatus);
-		LOGGER.debug(">>>> offerInvalid = " + offerInvalid);
-		LOGGER.debug(">>>> offerValid = " + offerValid);
-
-		LOGGER.debug(">>>> isNotOfferDateValid = " + isNotOfferDateValid);
-		LOGGER.debug(">>>> clientDBCurrDt = " + clientDBCurrDt);
-		LOGGER.debug(">>>> offer displayStartDt = " + displayStartDt);
-		LOGGER.debug(">>>> offer displayEndDt = " + displayEndDt);
-		
-		
-		if (isNotOfferDateValid) {
+		if (clientDBCurrDt.before(displayStartDt) || clientDBCurrDt.after(displayEndDt)) {
+			
 			isAcceptableOffer = false;
-		} else {
-		
-				//Clipped offer
-				if(ClipStatus.ADDED_TO_CARD.equalsIgnoreCase(clipStatus)) {
-					if(offerInvalid && isNotOfferDateValid) {//offer is clipped: Offer Status in  “L,D,E etc”:  end date of offer passed : not show Offer
-						isAcceptableOffer = false;
-					} 
-					
-					if (offerValid && isNotOfferDateValid) { //offer is clipped: Offer Status is A:  end date of offer passed 
-						isAcceptableOffer = false;
-					}
-					
-				}
-				
-				//Unclipped offer
-				if(ClipStatus.UNCLIPPED.equalsIgnoreCase(clipStatus)) {
-					
-					if(offerInvalid && !isNotOfferDateValid) {//offer is Unclipped: Offer Status in “L,D,E etc”: end date of offer not passed : don’t show Offer
-						isAcceptableOffer = false;
-					} 
-					
-					if (offerValid && isNotOfferDateValid) { //offer is Unclipped: Offer Status is A:  end date of offer passed : don’t show Offer 
-							isAcceptableOffer = false;
-					}
-				}
-		}
-		
-		LOGGER.debug(">>>> isAcceptableOffer = " + isAcceptableOffer);
-		
-
-		/*if (offerInvalid && ClipStatus.UNCLIPPED.equalsIgnoreCase(clipStatus)) {
+			
+		} else if (offerInvalid && ClipStatus.UNCLIPPED.equalsIgnoreCase(clipStatus)) {
+			
 			isAcceptableOffer = false;
-		} //else if (!offerInvalid && ClipStatus.UNCLIPPED.equalsIgnoreCase(clipStatus)) {
-			else if (!offerInvalid) {
-				if (clientDBCurrDt.before(displayStartDt) || clientDBCurrDt.after(displayEndDt)) {
-					isAcceptableOffer = false;
-				}
-		}*/
-		
-		if(!isAcceptableOffer) {
-			return null;
 		}
 
-		AllocatedOffer offer = new AllocatedOffer();
-		
-		offer.setOfferId(offerDetail.getOfferId());
-		offer.setOfferPgm(offerDetail.getOfferProgramCd());
-		
-
-		offer.setOfferTs(new Timestamp(offerDetail.getLastUpdateTs().getTime()));
-		
-
-		offer.setShoppingListCategoryId(offerDetail.getPrimaryCategoryId());
-		
-		
-		offer.setClipStatus(clipStatus);
-		offer.setClipId(offerClipStatus.getOfferId()); // Check with Efren . It should be a String but is Long now.
-		//TODO: Change to ClipId. per Arun
-		
-		offer.setPurchaseInd(PurchaseIndicator.PURCHASE_IND_BOUGHT);
-		
-		// Allocated Offer Details
-		
-	
-		offer.getOfferDetail().setStartDt(displayStartDt);
-		offer.getOfferDetail().setEndDt(offerEndDt);
-		
-		Double regularPrice = null;
-		
-		if (null != offerStorePrice) {
-			regularPrice = offerStorePrice.getRegularPrice();
+		if (isAcceptableOffer) {
+			offer = new AllocatedOffer();		
+			offer.setOfferId(offerDetail.getOfferId());
+			offer.setOfferPgm(offerDetail.getOfferProgramCd());
+			offer.setOfferTs(new Timestamp(offerDetail.getLastUpdateTs().getTime()));
+			offer.setShoppingListCategoryId(offerDetail.getPrimaryCategoryId());
+			offer.setClipStatus(clipStatus);
+			offer.setClipId(offerClipStatus.getOfferId());
+			offer.setPurchaseInd(PurchaseIndicator.PURCHASE_IND_BOUGHT);
+			offer.getOfferDetail().setStartDt(displayStartDt);
+			offer.getOfferDetail().setEndDt(offerEndDt);
+			Double regularPrice = null;
+			if (null != offerStorePrice) {
+				regularPrice = offerStorePrice.getRegularPrice();
+			}
+			PDPricing pDPricing = PDPricingHelper.analyzePricing(offerDetail.getOfferPrice(), regularPrice);
+			offer.getOfferDetail().setPriceTitle2(pDPricing.getPriceTitle());
+			offer.getOfferDetail().setPriceTitle2Type(pDPricing.getPriceTitleType());
+			offer.getOfferDetail().setPriceValue2(pDPricing.getPriceValue());
 		}
 
-		if (LOGGER.isInfoEnabled()) {
-			LOGGER.info("currentOfferId " + offerDetail.getOfferId());
-			LOGGER.info("dOfferPrice " + offerDetail.getOfferPrice());
-			LOGGER.info("dRegPrice " + regularPrice);
-		}
 		
-		
-		
-		PDPricing pDPricing = PDPricingHelper.analyzePricing(offerDetail.getOfferPrice(), regularPrice);
-		
-		offer.getOfferDetail().setPriceTitle2(pDPricing.getPriceTitle());
-		offer.getOfferDetail().setPriceTitle2Type(pDPricing.getPriceTitleType());
-		offer.getOfferDetail().setPriceValue2(pDPricing.getPriceValue());
-		
-
-		
-	  return offer;
+		return offer;
 	
 	}
 
