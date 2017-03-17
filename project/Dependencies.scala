ff9adc5970bf278532b5d40c40d2098abaa2cf93
kbuen03 <karlo.buenaventura@safeway.com>
2017-03-17 11:40:00 +0800

    turn on debug mode

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,6 @@ object Dependencies {
     val commoncore	= "emju-common-core" % "emju-common-core_2.11" % "3.0.0"
     val commonauth	= "emju-common-auth" % "emju-common-auth_2.11" % "3.0.0"
     val commonmail	= "emju-common-mail" % "emju-common-mail_2.11" % "3.0.0"
-    val commoncache	= "emju-common-cache" % "emju-common-cache_2.11" % "3.0.0"
     val allocation	= "emju-allocation" % "emju-allocation_2.11" % "2.16.0"
   }
 
