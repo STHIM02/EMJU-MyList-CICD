6e3c42d53c9be2708c90cbf16fad6d33f91b3490
kbuen03 <karlo.buenaventura@safeway.com>
2017-03-20 07:20:02 +0800

    revert to common 2.10.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
   }
   
   object emju {
-    val common		= "emju-common" % "emju-common_2.11" % "2.15.0"
+    val common		= "emju-common" % "emju-common_2.11" % "2.10.1"
     val allocation	= "emju-allocation" % "emju-allocation_2.11" % "2.16.0"
   }
 
